c07c70d862c0e78ccb13fd426e185c6743d2e002
Alva Vially <alva@vially.fr>
2022-01-13 09:30:52 +0100

    Create database class

---
+++ Database.php
@@ -0,0 +1,54 @@
+<?php
+
+class Database
+{
+
+    private $db;
+
+    public function __construct()
+    {
+        try {
+            $this->db = new PDO('sqlite:' . $_ENV['SRH_DATABASE']);
+            echo 'Connection to DB \'' .  
+                $_ENV['SRH_DATABASE'] . 
+                '\' established' . 
+                PHP_EOL;
+        } catch (Exception $e) {
+            var_dump($e->getMessage());
+        }
+    }
+
+    public function createColumn(string $table, string $newColumn): void
+    {
+        $stmt = "ALTER TABLE $table ADD $newColumn TEXT";
+        $request = $this->db->prepare($stmt);
+        $request->execute();
+        echo 'Column "' . $newColumn . '" created' . PHP_EOL;
+    }
+
+    public function htmlLessCopy(
+        string $originalCol,
+        string $newCol,
+        string $table
+    ): void {
+        $stmt = "SELECT id, $originalCol FROM $table";
+        $request = $this->db->prepare($stmt);
+        $request->execute();
+        $articles = $request->fetchall();
+        $stmt = "UPDATE $table SET $newCol = ? WHERE id = ?";
+        $request = $this->db->prepare($stmt);
+        foreach ($articles as $article) {
+            $plaindesc = str_replace('<', ' <', $article['description']);
+            $plaindesc = strip_tags($plaindesc);
+            $plaindesc = preg_replace('/\s\s+/', ' ', $plaindesc);
+            $request->execute([
+                $plaindesc,
+                $article['id']
+            ]);
+            echo 'Row ' .
+                $article['id'] .
+                ' copied with HTML removed' .
+                PHP_EOL;
+        }
+    }
+}
